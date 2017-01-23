309eca27d32829a9f947efd3f1c0758cd75f3089
terrance <studyskill@126.com>
2017-01-23 13:09:57 +0800

    FutureTask提高加载需要的数据

---
+++ thread/src/main/java/ProductInfo/Preloader.java
@@ -0,0 +1,36 @@
+package ProductInfo;
+
+import ProductInfo.ProductInfo;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+/**
+ * Created by cq on 2017/1/23.
+ */
+public class Preloader {
+
+    private final FutureTask<ProductInfo> future = new FutureTask<ProductInfo>(new Callable<ProductInfo>() {
+        public ProductInfo call() throws Exception {
+            return loadProductInfo();
+        }
+    });
+
+
+    private final Thread thread = new Thread(future);
+
+    public void start() {
+        thread.start();
+    }
+
+    public ProductInfo get() {
+        try {
+            return future.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ thread/src/main/java/ProductInfo/ProductInfo.java
@@ -0,0 +1,7 @@
+package ProductInfo;
+
+/**
+ * Created by cq on 2017/1/23.
+ */
+public class ProductInfo {
+}
